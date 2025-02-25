cdb3e4886bec1e98ffec9885137560df4febbf51
vpopovic003 <vpopovic003@gmail.com>
2025-02-25 02:58:13 +0100

    add hero section

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,15 @@
 import "../styles/global.css";
 import "prismjs/themes/prism.css";
 import "prismjs/plugins/line-numbers/prism-line-numbers.css";
+import Nav from "@/components/Nav";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Nav />
+        {children}
+      </body>
     </html>
   );
 }
--- components/front/Hero.js
+++ components/front/Hero.js
@@ -1,10 +1,20 @@
 import styles from "@/styles/front/Hero.module.scss";
-import Nav from "@/components/Nav";
+import utilStyles from "../../styles/utils.module.css";
+import Image from "next/image";
 
 const Hero = () => {
   return (
     <div className={styles["hero"]}>
-      <Nav />
+      <div className={styles["hero__img-box"]}>
+        <Image
+          src={"/images/profile.png"}
+          alt={"Profile photo"}
+          width={150}
+          height={150}
+        />
+      </div>
+      <h2 className={utilStyles.headingLg}>Vladimir Popovic</h2>
+      <small className={utilStyles.lightText}>Frontend Developer</small>
     </div>
   );
 };
